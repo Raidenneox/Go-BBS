2e810a805a770fd9bf51e397aee198196930df54
Raidenneox <Raidennrk@outlook.com>
2022-09-19 09:47:46 +0800

    complete the prj

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"time"
 	"web_app/controller"
 	"web_app/logger"
 	"web_app/middlewares"
@@ -14,25 +15,31 @@ func Setup() *gin.Engine {
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 	v1 := r.Group("/v1/api")
 
+	v1.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
 	//登陆的处理函数
 	v1.POST("/signup", controller.SignUpHandler) //注册的处理函数
 	v1.POST("/login", controller.LoginHandler)
-	v1.Use(middlewares.JWTAuthMiddleware()) //应用JWT认证中间件
+	//令牌桶算法
+	//10个令牌,每两秒钟添加一个
 
 	{
 		v1.GET("/community", controller.CommunityHandler)
 		v1.GET("/community/:id", controller.CommunityDetailHandler)
-	}
-	{
-		v1.POST("/post", controller.CreatePostHandler)
 		v1.GET("/post/:id", controller.GetPostDetailHandler)
 		v1.GET("/posts", controller.GetPostListHandler)
 
 		//根据时间或分数获取帖子列表
 		v1.GET("/posts2", controller.GetPostListHandler2)
 	}
+	v1.Use(middlewares.JWTAuthMiddleware(), middlewares.RateLimitMiddleware(2*time.Second, 10)) //应用JWT认证中间件
+	{
+		v1.POST("/post", controller.CreatePostHandler)
+		v1.POST("/vote", controller.PostVoteHandler)
 
-	v1.POST("/vote", controller.PostVoteHandler)
+	}
 
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(200, gin.H{
